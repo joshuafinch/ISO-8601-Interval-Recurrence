7d6ed5a2b1f95c38954b45e2de2f490518d6fc40
Joshua Finch <josh@joshuafinch.com>
2015-07-23 17:51:43 +0100

    [WIP] Scan string

---
+++ ISO8601IntervalRecurrence/ISO8601StringParser.swift
@@ -0,0 +1,203 @@
+//
+//  ISO8601StringParser.swift
+//  ISO8601IntervalRecurrence
+//
+//  Created by Joshua Finch on 23/07/2015.
+//  Copyright (c) 2015 Joshua Finch. All rights reserved.
+//
+
+import Foundation
+
+public class ISO8601StringParser
+{
+    public static func parseISO8601StringIntoParts(iso8601string: String) -> (reccurence: String?, startDateTime: String?, endDateTime: String?, interval: String?)
+    {
+        var recurrence: String?
+        var interval: String?
+        var startDateTime: String?
+        var endDateTime: String?
+
+        let parts: [String] = iso8601string.componentsSeparatedByString("/")
+
+        for (index, part) in enumerate(parts)
+        {
+            let scanner = NSScanner(string: part)
+
+            if (scanner.scanString("R", intoString: nil))
+            {
+                // Part begins with R
+                recurrence = part
+            }
+            else if (scanner.scanString("P", intoString: nil))
+            {
+                // Part begins with P
+                interval = part
+            }
+            else
+            {
+                // Part does not begin with R or P, most likely a datetime
+
+                if (interval == nil && startDateTime == nil)
+                {
+                    startDateTime = part
+                }
+                else
+                {
+                    if (endDateTime == nil)
+                    {
+                        // Must be the end time
+                        endDateTime = part
+                    }
+                    else
+                    {
+                        println("Weird stuff happening, two end times?")
+                    }
+                }
+            }
+        }
+        
+        return (recurrence, startDateTime, endDateTime, interval)
+    }
+
+    // MARK: - 
+
+    // MARK: - Parse Parts
+
+    private static func parseRecurrenceAmount(recurrence: String) -> Int?
+    {
+        let scanner = NSScanner(string: recurrence)
+        if (scanner.scanString("R", intoString: nil))
+        {
+            var recurrenceAmount: Int = NSNotFound
+            if (scanner.scanInteger(&recurrenceAmount))
+            {
+                return recurrenceAmount
+            }
+        }
+
+        return nil
+    }
+
+    public static func parsePeriod(period: String) -> Float
+    {
+        var years: Float = 0
+        var months: Float = 0
+        var days: Float = 0
+        var hours: Float = 0
+        var minutes: Float = 0
+        var seconds: Float = 0
+        var weeks: Float = 0
+
+        func nextDateValue(scanner: NSScanner)
+        {
+            var periodValue: Float = 0
+            if (scanner.scanFloat(&periodValue))
+            {
+                if (scanner.scanString("Y", intoString: nil))
+                {
+                    years = periodValue
+                }
+                else if (scanner.scanString("M", intoString: nil))
+                {
+                    months = periodValue
+                }
+                else if (scanner.scanString("D", intoString: nil))
+                {
+                    days = periodValue
+                }
+            }
+        }
+
+        func nextTimeValue(scanner: NSScanner) -> Bool
+        {
+            var periodValue: Float = 0
+            if (scanner.scanFloat(&periodValue))
+            {
+                if (scanner.scanString("H", intoString: nil))
+                {
+                    hours = periodValue
+                }
+                else if (scanner.scanString("M", intoString: nil))
+                {
+                    minutes = periodValue
+                }
+                else if (scanner.scanString("S", intoString: nil))
+                {
+                    seconds = periodValue
+                }
+                else if (scanner.scanString("W", intoString: nil))
+                {
+                    weeks = periodValue
+                }
+            }
+            else
+            {
+                println("Couldn't scan a float in: \(scanner.string)")
+                return false
+            }
+
+            return true
+        }
+
+        let scanner = NSScanner(string: period)
+        if (scanner.scanString("P", intoString: nil))
+        {
+            var dateString: NSString? = ""
+            scanner.scanUpToString("T", intoString: &dateString)
+            scanner.scanString("T", intoString: nil)
+
+            let length = (scanner.string as NSString).length
+            let range = NSRange(location: scanner.scanLocation, length: length - scanner.scanLocation)
+            let timeString = (scanner.string as NSString).substringWithRange(range) as String
+
+            if (dateString?.length > 0)
+            {
+                let dateScanner = NSScanner(string: dateString! as String)
+                while (!dateScanner.atEnd)
+                {
+                    println("stuck on dateScanner?")
+                    nextDateValue(dateScanner)
+                }
+            }
+
+            if ((timeString as NSString).length > 0)
+            {
+                let timeScanner = NSScanner(string: timeString)
+                while(!timeScanner.atEnd)
+                {
+                    let hasNextTimeValue = nextTimeValue(timeScanner)
+                    if (!hasNextTimeValue)
+                    {
+                        println("no more time values? \(timeScanner.scanLocation), \((timeScanner.string as NSString).length)")
+                    }
+                }
+            }
+        }
+
+        let secondsInMilli = seconds * 1000
+        let minuteInMilli = minutes * 60 * 1000
+        let hourInMilli = hours * 60 * 60 * 1000
+
+        let mhsInMilli = secondsInMilli + minuteInMilli + hourInMilli
+
+        let yearsInMonths = years * 12
+        let yearsAndMonthsInYears = (yearsInMonths + months) / 12.0
+        let yearsAndMonthsInDays = (yearsAndMonthsInYears * 14097.0) / 400.0
+
+        let weeksInDays = weeks * 7
+        let yearsAndMonthsAndWeeksAndDaysInDays = yearsAndMonthsInDays + weeksInDays + days
+
+        return (yearsAndMonthsAndWeeksAndDaysInDays * 24 * 60 * 60 * 1000) + mhsInMilli
+    }
+
+    private static func parseDateTime(datetime: String) -> NSDateComponents?
+    {
+        //TODO: Check this is a dateTime string, not a P string or R string or something else
+
+        var components = NSDateComponents()
+
+        //TODO: Extract date and time components
+
+        return components
+    }
+}
--- ISO8601IntervalRecurrenceTests/ISO8601IntervalRecurrenceTests.swift
+++ ISO8601IntervalRecurrenceTests/ISO8601IntervalRecurrenceTests.swift
@@ -9,28 +9,90 @@
 import UIKit
 import XCTest
 
-class ISO8601IntervalRecurrenceTests: XCTestCase {
-    
-    override func setUp() {
+import ISO8601IntervalRecurrence
+
+class ISO8601IntervalRecurrenceTests: XCTestCase
+{
+    override func setUp()
+    {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown()
+    {
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    func testPeriod()
+    {
+        let period = "PT1S"
+        let milliseconds = ISO8601StringParser.parsePeriod(period)
+
+        XCTAssertEqual(milliseconds, 1000, "milliseconds don't match")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+
+    // MARK: -
+
+    func testRecurrenceStartTimeEndTime()
+    {
+        assertPartsMatchString("R12/19850412T232050/19850625T103000", parts: (reccurence: "R12", startDateTime: "19850412T232050", endDateTime: "19850625T103000", interval: nil))
+    }
+
+    func testRecurrencePeriod()
+    {
+        assertPartsMatchString("R12/P2Y10M15DT10H30M20S", parts: (reccurence: "R12", startDateTime: nil, endDateTime: nil, interval: "P2Y10M15DT10H30M20S"))
+    }
+
+    func testRecurrenceStartTimePeriod()
+    {
+        assertPartsMatchString("R12/19850412T232050/P1Y2M15DT12H30M0S", parts: (reccurence: "R12", startDateTime: "19850412T232050", endDateTime: nil,  interval: "P1Y2M15DT12H30M0S"))
+    }
+
+    func testRecurrencePeriodEndTime()
+    {
+        assertPartsMatchString("R12/P1Y2M15DT12H30M0S/19850412T232050", parts: (reccurence: "R12", startDateTime: nil, endDateTime: "19850412T232050", interval: "P1Y2M15DT12H30M0S"))
+    }
+
+    func assertPartsMatchString(string: String, parts: (reccurence: String?, startDateTime: String?, endDateTime: String?, interval: String?))
+    {
+        let newParts = ISO8601StringParser.parseISO8601StringIntoParts(string)
+
+        if (parts.reccurence != nil)
+        {
+            XCTAssert(parts.reccurence == newParts.reccurence)
+        }
+        else
+        {
+            XCTAssertNil(newParts.reccurence)
+        }
+
+
+        if (parts.startDateTime != nil)
+        {
+            XCTAssert(parts.startDateTime == newParts.startDateTime)
+        }
+        else
+        {
+            XCTAssertNil(newParts.startDateTime)
+        }
+
+
+        if (parts.endDateTime != nil)
+        {
+            XCTAssert(parts.endDateTime == newParts.endDateTime)
+        }
+        else
+        {
+            XCTAssertNil(newParts.endDateTime)
+        }
+
+        if (parts.interval != nil)
+        {
+            XCTAssert(parts.interval == newParts.interval)
+        }
+        else
+        {
+            XCTAssertNil(newParts.interval)
         }
     }
-    
 }
