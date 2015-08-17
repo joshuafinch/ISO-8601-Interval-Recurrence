2fab53f7a8743f72147eb23d055081eaf224003a
Joshua Finch <josh@joshuafinch.com>
2015-08-17 11:51:49 +0100

    [BUGFIX] Fix scanner getting stuck in infinite loop when it cannot process any date/time values, also process period weeks correctly

--- ISO8601IntervalRecurrenceTests/ISO8601IntervalRecurrenceTests.swift
+++ ISO8601IntervalRecurrenceTests/ISO8601IntervalRecurrenceTests.swift
@@ -27,11 +27,51 @@ class ISO8601IntervalRecurrenceTests: XCTestCase
 
     func testPeriod()
     {
-        let period = "P3DT3S1W"
+        let period = "P3DT3S"
         let seconds = ISO8601StringParser.parsePeriod(period)
 
-        let mySeconds: Float = (24*60*60*10)+3
+        let mySeconds: Float = (24*60*60*3)+3
         XCTAssertEqual(seconds, mySeconds, "seconds don't match")
+    }
+
+    func testWeekPeriod()
+    {
+        let period = "P1W"
+        let seconds = ISO8601StringParser.parsePeriod(period)
+
+        let mySeconds: Float = (24*60*60*7)
+        XCTAssertEqual(seconds, mySeconds, "seconds don't match")
+    }
+
+    func testAllPeriodValuesUsed()
+    {
+        let aPeriod = "P2Y2M2W2DT2H2M2S"
+        let aSeconds = ISO8601StringParser.parsePeriod(aPeriod)
+
+        let seconds: Float = 2
+        let minutes: Float = 2
+        let hours: Float = 2
+        let days: Float = 2
+        let weeks: Float = 2
+        let months: Float = 2
+        let years: Float = 2
+
+        let secondsInSec = seconds
+        let minuteInSec = minutes * 60
+        let hourInSec = hours * 60 * 60
+
+        let mhsInSec = secondsInSec + minuteInSec + hourInSec
+
+        let yearsInMonths = years * 12
+        let yearsAndMonthsInYears = (yearsInMonths + months) / 12.0
+        let yearsAndMonthsInDays = (yearsAndMonthsInYears * 14097.0) / 400.0
+
+        let weeksInDays = weeks * 7
+        let yearsAndMonthsAndWeeksAndDaysInDays = yearsAndMonthsInDays + weeksInDays + days
+
+        let mySeconds: Float = (yearsAndMonthsAndWeeksAndDaysInDays * 24 * 60 * 60) + mhsInSec
+
+        XCTAssertEqual(aSeconds, mySeconds, "seconds don't match")
     }
 
     // MARK: - 
@@ -60,6 +100,11 @@ class ISO8601IntervalRecurrenceTests: XCTestCase
     func testRecurrencePeriod()
     {
         assertPartsMatchString("R12/P2Y10M15DT10H30M20S", parts: (reccurence: "R12", startDateTime: nil, endDateTime: nil, interval: "P2Y10M15DT10H30M20S"))
+    }
+
+    func testPeriodWithWeek()
+    {
+        assertPartsMatchString("R12/P1W", parts: (reccurence: "R12", startDateTime: nil, endDateTime: nil, interval: "P1W"))
     }
 
     func testRecurrenceStartTimePeriod()
